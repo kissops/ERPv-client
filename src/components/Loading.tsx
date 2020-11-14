14e105cbe9352ef50e5faf38f2493558c03ed6d2
Um9i <vulgarmessiah@gmail.com>
2020-11-14 16:20:38 +0000

    improve loading display

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,9 +3,10 @@ import React from "react";
 class Loading extends React.Component {
   render() {
     return (
-        <div className="my-3 bg-dark">
-            <div className="spinner-border m-3 " role="status"></div>
-        </div>
+      <div className="d-flex align-items-center bg-dark px-4 py-3 my-3">
+        Loading...
+        <div className="spinner-border spinner-border-sm ml-auto" role="status" aria-hidden="true"></div>
+      </div>
     );
   }
 }
